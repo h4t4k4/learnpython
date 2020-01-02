4dc9eef061cb98c93a0e3c88a10d4edfb2c327f2
h4t4k4 <h4t4k4@gmail.com>
2020-01-02 16:42:19 +0700

    2Jan20

---
+++ 2.py
@@ -0,0 +1,35 @@
+import sys
+
+file_name=""
+file_text=""
+file_finish=""
+
+try:
+    #Open File Stream
+    file=open(file_name,"w")
+except IOError:
+    print ("There was an error writing to ", file_name)
+    sys.exit()
+print ("Enter '",file_finish)
+print ("' When finished")
+while file_text != file_finish:
+    file_text=input("Enter text: ")
+    if file_text==file_finish:
+        #close the file
+        file.close
+        break
+    file.write(file_text)
+    file.write("\n")
+file.close()
+file_name=input("Enter filename: ")
+if len(file_name)==0:
+    print ("next time pleas enter something")
+    sys.exit()
+try:
+    file=open(file_name,"r")
+except IOError:
+    print ("There was an error reading file")
+    sys.exit()
+file_text=file.read()
+file.close()
+print (file_text)
